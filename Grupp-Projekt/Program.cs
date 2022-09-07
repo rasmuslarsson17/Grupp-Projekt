ef5b7fb2fa2f790112128c515e1b2ea58a01e979
johan <johannescrantz@yahoo.se>
2022-09-07 19:26:01 +0200

    Ny klass

--- Grupp-Projekt/Program.cs
+++ Grupp-Projekt/Program.cs
@@ -16,7 +16,7 @@ while (true)
     switch (val)
     {
         case 1:
-            ListProcesses();
+            SystemInformation.ListProcesses();
             break;
 
         case 2:
@@ -32,30 +32,33 @@ while (true)
     Console.ReadKey();
 }
 
-static void ListProcesses()
+public static class SystemInformation
 {
-    Process[] procList = Process.GetProcesses();
-    int i = 0, longestProcessNameLength = 0, longestProcessIdLength = 0;
-
-    // Kollar hur långa de längsta processnamnen och id:n i listan är, för formattering av utskrifterna
-    foreach (var process in procList)
+    public static void ListProcesses()
     {
-        if (process.ProcessName.Length > longestProcessNameLength)
-            longestProcessNameLength = process.ProcessName.Length;
-        if (process.Id.ToString().Length > longestProcessIdLength)
-            longestProcessIdLength = process.Id.ToString().Length;
-    }
+        Process[] procList = Process.GetProcesses();
+        int i = 0, longestProcessNameLength = 0, longestProcessIdLength = 0;
 
-    // Utskrift
-    foreach (var process in procList)
-    {
-        Console.Write($"\n{process.ProcessName}");
-        Console.SetCursorPosition(longestProcessNameLength + longestProcessIdLength - process.Id.ToString().Length + 5, Console.GetCursorPosition().Top);
-        Console.Write($"{process.Id}");
-
-        if ((++i % 5) == 0 || i == procList.Length)
+        // Kollar hur långa de längsta processnamnen och id:n i listan är, för formattering av utskrifterna
+        foreach (var process in procList)
         {
-            Console.WriteLine();
+            if (process.ProcessName.Length > longestProcessNameLength)
+                longestProcessNameLength = process.ProcessName.Length;
+            if (process.Id.ToString().Length > longestProcessIdLength)
+                longestProcessIdLength = process.Id.ToString().Length;
+        }
+
+        // Utskrift
+        foreach (var process in procList)
+        {
+            Console.Write($"\n{process.ProcessName}");
+            Console.SetCursorPosition(longestProcessNameLength + longestProcessIdLength - process.Id.ToString().Length + 5, Console.GetCursorPosition().Top);
+            Console.Write($"{process.Id}");
+
+            if ((++i % 5) == 0 || i == procList.Length)
+            {
+                Console.WriteLine();
+            }
         }
     }
 }
