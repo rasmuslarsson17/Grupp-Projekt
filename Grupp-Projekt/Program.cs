6322d84f8c228cf7c762995bcbf5d59586e1586e
M Berntson <112472542+JediMB@users.noreply.github.com>
2022-09-07 13:30:06 +0200

    Separerade processlistan till en egen metod och gjorde så att man kommer tillbaka till menyn med en tangenttryckning istället för automatiskt.

--- Grupp-Projekt/Program.cs
+++ Grupp-Projekt/Program.cs
@@ -2,6 +2,7 @@
 
 while (true)
 {
+    Console.Clear();
     Console.WriteLine("1 : Skriv ut alla processer\n2 : Avsluta");
     
     if(!Int32.TryParse(Console.ReadKey().KeyChar.ToString(), out int val))
@@ -15,29 +16,7 @@ while (true)
     switch (val)
     {
         case 1:
-            Process[] procList = Process.GetProcesses();
-            int i = 0, longestProcessNameLength = 0, longestProcessIdLength = 0;
-
-            // Kollar hur långa de längsta processnamnen och id:n i listan är, för formattering av utskrifterna
-            foreach (var process in procList)
-            {
-                if (process.ProcessName.Length > longestProcessNameLength)
-                    longestProcessNameLength = process.ProcessName.Length;
-                if (process.Id.ToString().Length > longestProcessIdLength)
-                    longestProcessIdLength = process.Id.ToString().Length;
-            }
-
-            // Utskrift
-            foreach (var process in procList) {
-                Console.Write($"\n{process.ProcessName}");
-                Console.SetCursorPosition(longestProcessNameLength + longestProcessIdLength - process.Id.ToString().Length + 5, Console.GetCursorPosition().Top);
-                Console.Write($"{process.Id}");
-
-                if((++i % 5) == 0 || i == procList.Length)
-                {
-                    Console.WriteLine();
-                }
-            }
+            ListProcesses();
             break;
 
         case 2:
@@ -49,4 +28,34 @@ while (true)
     }
 
     Console.WriteLine("\n");
+    Console.Write("Tryck på valfri tangent för att återvända till menyn... ");
+    Console.ReadKey();
+}
+
+static void ListProcesses()
+{
+    Process[] procList = Process.GetProcesses();
+    int i = 0, longestProcessNameLength = 0, longestProcessIdLength = 0;
+
+    // Kollar hur långa de längsta processnamnen och id:n i listan är, för formattering av utskrifterna
+    foreach (var process in procList)
+    {
+        if (process.ProcessName.Length > longestProcessNameLength)
+            longestProcessNameLength = process.ProcessName.Length;
+        if (process.Id.ToString().Length > longestProcessIdLength)
+            longestProcessIdLength = process.Id.ToString().Length;
+    }
+
+    // Utskrift
+    foreach (var process in procList)
+    {
+        Console.Write($"\n{process.ProcessName}");
+        Console.SetCursorPosition(longestProcessNameLength + longestProcessIdLength - process.Id.ToString().Length + 5, Console.GetCursorPosition().Top);
+        Console.Write($"{process.Id}");
+
+        if ((++i % 5) == 0 || i == procList.Length)
+        {
+            Console.WriteLine();
+        }
+    }
 }
